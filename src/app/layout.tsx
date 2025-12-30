d301ada9eb3d036d7d3e6e80fff85ba6832f6c95
prashant.jangam <prashant.jangam@vodafone.com>
2025-12-30 19:31:05 +0530

    code updated.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Devanagari:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
-        <div className="flex flex-col min-h-screen">
+      <body className="font-body min-w-screen antialiased">
+        <div className="flex flex-col min-h-screen min-w-screen">
           <ClientProviders>
             <Header />
             <main className="flex-grow flex justify-center">
